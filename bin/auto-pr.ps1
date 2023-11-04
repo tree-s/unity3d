9a4e0b274165055d408abe1664a69c6855ae6066
Michael J. MacMullin <mike.macmullin@gmail.com>
2023-11-04 21:18:48 -0230

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "tree-s/unity3d:main"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
